31946db389cccd68683e1f6820cadae5a06179ad
Henrik Nørgaard <menneskefernis@gmail.com>
2019-12-17 00:57:40 +0100

    finished excercises

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -31,6 +31,23 @@ def save_thank_you_letter(id, form_letter)
   end
 end
 
+def clean_phone_number(phone_number)
+  number = phone_number.gsub(/[\.( )-]/, '')
+
+  case true
+  when number.size < 10
+    "N/A"
+  when number.size > 11
+    "N/A"
+  when number.size == 11 && !number[0].to_i == 1
+    "N/A"
+  when number.size == 11 && number[0].to_i == 1
+    number.slice(0, 10)
+  else
+    number
+  end
+end
+
 puts "EventManager Initialized!"
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
@@ -38,11 +55,58 @@ contents = CSV.open "event_attendees.csv", headers: true, header_converters: :sy
 template_letter = File.read "form_letter.erb"
 erb_template = ERB.new template_letter
 
+$registration_hours = Hash.new(0)
+$registration_weekdays = Hash.new(0)
+
+def save_registration_hour(date)
+  format = '%m/%d/%Y %H:%M'
+  $registration_hours[DateTime.strptime(date, format).hour] += 1
+end
+
+def save_weekday_registration(date)
+  format = '%m/%d/%Y'
+  $registration_weekdays[DateTime.strptime(date, format).wday] += 1
+end
+
+def calculate_peak(hash)
+  #hash.max_by{|k,v| v}[0]
+  
+  hash = hash.sort_by {|k,v| v}.reverse
+  [hash[0][0], hash[1][0]]
+end
+
+def num_to_weekday(number)
+  case number
+  when 0
+    "Sunday"
+  when 1
+    "Monday"
+  when 2
+    "Tuesday"
+  when 3
+    "Wednesday"
+  when 4
+    "Thursday"
+  when 5
+    "Friday"
+  when 6
+    "Saturday"
+  end
+end
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   
   zipcode = clean_zipcode(row[:zipcode])
+
+  phone_number = clean_phone_number(row[:homephone])
+
+  registration_date = row[:regdate]
+  
+  save_registration_hour(registration_date)
+
+  save_weekday_registration(registration_date)
 
   legislators = legislators_by_zipcode(zipcode)
 
@@ -51,3 +115,6 @@ contents.each do |row|
   save_thank_you_letter(id, form_letter)
 end
 
+peak_days = calculate_peak($registration_weekdays)
+puts "Peak days are: #{peak_days.map { |day| num_to_weekday(day) }.join(', ')}"
+puts "Peak hours are: #{calculate_peak($registration_hours).join(', ')}"
